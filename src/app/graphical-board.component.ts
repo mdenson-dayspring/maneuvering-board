6b4e1ca83f0ea7648c77d8abe61e3433fb534b7a
Matthew Denson <mdenson@dayspring-tech.com>
2016-12-22 16:39:50 -0800

    Handled vertical (near vertical) case.

--- src/app/graphical-board.component.ts
+++ src/app/graphical-board.component.ts
@@ -36,7 +36,7 @@ export class GraphicalBoardDirective implements AfterViewInit {
 
   constructor(private $contactList: ContactListService, private $log: Logger) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.contactList$ = this.$contactList.contactList;
     this.contactList = [];
 
@@ -44,12 +44,12 @@ export class GraphicalBoardDirective implements AfterViewInit {
       this.contactList = updatedContacts;
 
       if (this.canvas) {
-        this.contactList.forEach((c) => this.drawContact(c));
+        this.redraw();
       }
     });
   }
 
-  ngAfterViewInit() {
+  ngAfterViewInit(): void {
     this.canvas = this.canvasRef.nativeElement;
     let erd = elementResizeDetectorMaker({
       strategy: 'scroll'
@@ -61,18 +61,31 @@ export class GraphicalBoardDirective implements AfterViewInit {
 
       this.rescale();
 
-      this.drawGrid();
-      this.contactList.forEach((c) => this.drawContact(c));
+      this.redraw();
     });
   }
 
-  private rescale() {
+  private rescale(): void {
       this.uisize = Math.min(this.canvas.height, this.canvas.width);
       this.ctr = this.uisize / 2;
       this.unit = (this.uisize - 22) / 20;
       this.tenth = (this.uisize - 22) / 200;
   }
-  private drawPoint(pt: UIPoint) {
+  private redraw(): void {
+      this.drawGrid();
+      this.contactList.forEach((c) => this.drawPoint(c.toUiPoint()));
+
+      this.drawLine(this.contactList[0].toUiPoint(), this.contactList[2].toUiPoint(), false);
+
+      let pte = new Contact('e', 0, 0, 0);
+      let ptr = new Contact('r', 0, 150, 6);
+      let ptm = new Contact('m', 0, 99, 9);
+      this.drawVector(pte, ptr);
+      this.drawVector(pte, ptm);
+      this.drawVector(ptr, ptm);
+  }
+
+  private drawPoint(pt: UIPoint): void {
     if (this.canvas.getContext) {
       let ctx = this.canvas.getContext('2d');
       ctx.strokeStyle = 'black';
@@ -80,8 +93,6 @@ export class GraphicalBoardDirective implements AfterViewInit {
       ctx.lineWidth = 1;
 
       ctx.translate(this.ctr, this.ctr);
-
-      ctx.lineWidth = 1;
 
       ctx.beginPath();
       ctx.moveTo(pt.x * this.unit, (-pt.y * this.unit) - 1);
@@ -101,12 +112,69 @@ export class GraphicalBoardDirective implements AfterViewInit {
       ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
   }
+  private drawLine(pt1: UIPoint, pt2: UIPoint, segment: boolean = true): UIPoint {
+    let ptend = pt2;
+    if (this.canvas.getContext) {
+      let ctx = this.canvas.getContext('2d');
+      ctx.strokeStyle = 'black';
+      ctx.fillStyle = 'black';
+      ctx.lineWidth = 1;
 
-  private drawContact(m: Contact) {
-    this.drawPoint(m.toUiPoint());
+      ctx.translate(this.ctr, this.ctr);
+
+      if (!segment) {
+        ptend = new UIPoint();
+        if (Math.abs(pt2.x - pt1.x) > 0.01 &&
+            Math.abs((pt2.y - pt1.y) / (pt2.x - pt1.x)) <= 1) {
+          ptend.x = (pt1.x < pt2.x) ? 15 : -15;
+          ptend.y = (ptend.x - pt1.x) * (pt2.y - pt1.y) / (pt2.x - pt1.x) + pt1.y;
+        } else {
+          ptend.y = (pt1.y < pt2.y) ? 15 : -15;
+          ptend.x = (ptend.y - pt1.y) * (pt2.x - pt1.x) / (pt2.y - pt1.y) + pt1.x;
+        }
+      }
+
+      ctx.beginPath();
+      ctx.moveTo(pt1.x * this.unit, (-pt1.y * this.unit));
+      ctx.lineTo(ptend.x * this.unit, (-ptend.y * this.unit));
+      ctx.stroke();
+
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }
+    return ptend;
+  }
+  private drawVector(ptBase: Contact, ptVect: Contact, segment: boolean = true): void {
+    if (this.canvas.getContext) {
+      let pt1 = ptBase.toUiPoint();
+      let ptend = this.drawLine(pt1, ptVect.toUiPoint(), true);
+
+      let ctx = this.canvas.getContext('2d');
+      ctx.strokeStyle = 'black';
+      ctx.fillStyle = 'black';
+      ctx.font = '6pt Arial';
+      ctx.lineWidth = 1;
+
+      ctx.translate(this.ctr, this.ctr);
+
+      ctx.beginPath();
+      let metrics = ctx.measureText(pt1.label);
+      ctx.fillText(pt1.label,
+        (pt1.x * this.unit) - metrics.width - this.tenth,
+        (-pt1.y * this.unit) - this.tenth);
+      ctx.stroke();
+
+      ctx.beginPath();
+      metrics = ctx.measureText(ptend.label);
+      ctx.fillText(ptend.label,
+        (ptend.x * this.unit) - metrics.width - this.tenth,
+        (-ptend.y * this.unit) - this.tenth);
+      ctx.stroke();
+
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }
   }
 
-  private drawGrid() {
+  private drawGrid(): void {
     if (this.canvas.getContext) {
       let ctx = this.canvas.getContext('2d');
 
--- src/app/services/contact-list.service.ts
+++ src/app/services/contact-list.service.ts
@@ -26,81 +26,21 @@ export class ContactListService {
         this.dataStore.contactList = [
             new Contact(
                 'M1',
-                2200,
-                67,
-                10.8
+                1100,
+                255,
+                10
             ),
             new Contact(
                 'M2',
-                2208,
-                63,
-                9.4
+                1107,
+                260,
+                7.85
             ),
             new Contact(
                 'M3',
-                2216,
-                58,
-                8.2
-            ),
-            new Contact(
-                'M4',
-                2224,
-                52,
-                7.2
-            ),
-            new Contact(
-                'M5',
-                2232,
-                43,
-                6.2
-            ),
-            new Contact(
-                '6.2@100T',
-                2232,
-                100,
-                6.2
-            ),
-            new Contact(
-                '6.2@170T',
-                2232,
-                170,
-                6.2
-            ),
-            new Contact(
-                '3.2@180T',
-                2232,
-                180,
-                3.2
-            ),
-            new Contact(
-                '9.2@190T',
-                2232,
-                190,
-                9.2
-            ),
-            new Contact(
-                '5.2@250T',
-                2232,
-                250,
-                5.2
-            ),
-            new Contact(
-                '2@270T',
-                2232,
+                1114,
                 270,
-                2
-            ),
-            new Contact(
-                '8.2@280T',
-                2232,
-                280,
-                8.2
-            ),
-            new Contact(
-                '4@350T',
-                2232,
-                350,
-                4
+                5.6
             )
         ];
         this._contactList.next(Object.assign({}, this.dataStore).contactList);
